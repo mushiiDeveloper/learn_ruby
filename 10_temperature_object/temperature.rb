3cdfa44771abedc26378401fbbc9ec0c4e263e5d
mushiiDeveloper <vovik.mushiy@gmail.com>
2018-07-17 16:04:42 +0300

    Finish 10 exam

--- 10_temperature_object/temperature.rb
+++ 10_temperature_object/temperature.rb
@@ -1,2 +1,41 @@
 class Temperature
+
+  def initialize(hash)
+    @f_temp, @c_temp = hash[:f], hash[:c]
+    @f_temp = (@c_temp.to_f * 9 / 5.0) + 32 unless @c_temp.nil?
+    @c_temp = (@f_temp - 32) * 5 / 9.0 unless @f_temp.nil?
+  end
+
+  def in_fahrenheit
+    @f_temp
+  end
+
+  def in_celsius
+    @c_temp
+  end
+
+  def self.from_celsius(temp)
+    self.new(:c => temp)
+  end
+
+  def self.from_fahrenheit(temp)
+    self.new(:f => temp)
+  end
 end
+
+  class Celsius < Temperature
+
+    def initialize(temp)
+      @c_temp = temp
+      @f_temp = (@c_temp.to_f * 9 / 5) + 32 unless @c_temp.nil?
+    end
+
+  end
+
+  class Fahrenheit < Temperature
+
+    def initialize(temp)
+      @f_temp = temp
+      @c_temp = (@f_temp - 32) * 5 / 9 unless @f_temp.nil?
+    end
+  end
