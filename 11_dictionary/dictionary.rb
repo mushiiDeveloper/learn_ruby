8ae8c7b0875012019518d205cade9c35c546493c
mushiiDeveloper <vovik.mushiy@gmail.com>
2018-07-17 16:50:55 +0300

    Finish 11 exam

---
+++ 11_dictionary/dictionary.rb
@@ -0,0 +1,64 @@
+class Dictionary
+
+  def initialize
+    @entries = {}
+  end
+
+  def entries
+    @entries
+  end
+
+  def add(entry)
+
+    case
+    when
+      entry.class == Hash
+      key = nil
+      value = nil
+      entry.each_key {|x| key = x}
+      entry.each_value {|y| value = y}
+      @entries[key] = value
+    when entry.class == String
+      @entries[entry] = nil
+    end
+  end
+
+  def keywords
+    @entries.keys.sort
+  end
+
+  def include?(key)
+    @entries.each_key do |x|
+      return true if x == key
+    end
+    false
+  end
+
+  def find(key)
+    keys = []
+    entries.each_key do |x|
+      if /#{key}/.match(x)
+        puts x
+        keys << x
+      end
+    end
+
+    if keys.length > 0
+      output = Hash.new
+      keys.each do |x|
+        output[x] = @entries[x]
+      end
+      output
+    else
+      {}
+    end
+  end
+
+  def printable
+    a = @entries.sort.map do |key,value|
+      "[#{key}] \"#{value}\""
+
+    end
+    a.join("\n")
+  end
+end
